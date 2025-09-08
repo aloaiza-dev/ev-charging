89a2ef2c687cdd98b15797a41090971f2203699f
Alex Loaiza <alex.loaiza@gmail.com>
2025-09-07 18:22:53 -0600

    fix: fixing file path

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 // super-minimal offline cache for shell + icons
 const CACHE = 'ev-calc-v1';
 const ASSETS = [
-  '/',                 // your index
-  '/manifest.webmanifest',
-  '/icons/ev-152.png',
-  '/icons/ev-180.png',
-  '/icons/ev-192.png',
-  '/icons/ev-512.png'
+  '',                 // your index
+  'manifest.webmanifest',
+  'icons/ev-152.png',
+  'icons/ev-180.png',
+  'icons/ev-192.png',
+  'icons/ev-512.png'
 ];
 
 self.addEventListener('install', e => {
